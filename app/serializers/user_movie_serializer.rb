474587594e0590de4bbdd82a0c6b7750a004062e
Kudzanayi Dzvairo <kudzanayi@Kudzanayis-MacBook-Air.local>
2019-03-27 12:29:20 -0400

    favorite in user-serializer

--- app/serializers/user_movie_serializer.rb
+++ app/serializers/user_movie_serializer.rb
@@ -1,5 +1,5 @@
 class UserMovieSerializer < ActiveModel::Serializer
-  attributes :id, :rating, :review, :user_id, :movie_id
+  attributes :id, :rating, :review, :favorites, :user_id, :movie_id
   belongs_to :user
   belongs_to :movie
 end
